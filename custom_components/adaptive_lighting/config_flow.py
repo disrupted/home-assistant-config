ed8ec7925df35e34802d363052e9208fba97d0f7
disrupted <hi@salomonpopp.me>
2020-10-06 00:52:51 +0200

    Update Adaptive Lighting integration

--- custom_components/adaptive_lighting/config_flow.py
+++ custom_components/adaptive_lighting/config_flow.py
@@ -4,6 +4,7 @@ import logging
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 
@@ -14,6 +15,7 @@ from .const import (  # pylint: disable=unused-import
     NONE_STR,
     VALIDATION_TUPLES,
 )
+from .switch import _supported_features
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -28,24 +30,24 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            await self.async_set_unique_id(user_input["name"])
+            await self.async_set_unique_id(user_input[CONF_NAME])
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=user_input["name"], data=user_input)
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required("name"): str}),
+            data_schema=vol.Schema({vol.Required(CONF_NAME): str}),
             errors=errors,
         )
 
     async def async_step_import(self, user_input=None):
         """Handle configuration by yaml file."""
-        await self.async_set_unique_id(user_input["name"])
+        await self.async_set_unique_id(user_input[CONF_NAME])
         for entry in self._async_current_entries():
             if entry.unique_id == self.unique_id:
                 self.hass.config_entries.async_update_entry(entry, data=user_input)
                 self._abort_if_unique_id_configured()
-        return self.async_create_entry(title=user_input["name"], data=user_input)
+        return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
     @staticmethod
     @callback
@@ -89,10 +91,12 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             if not errors:
                 return self.async_create_entry(title="", data=user_input)
 
-        all_lights = sorted(self.hass.states.async_entity_ids("light"))
-        to_replace = {
-            CONF_LIGHTS: cv.multi_select(all_lights),
-        }
+        all_lights = [
+            light
+            for light in self.hass.states.async_entity_ids("light")
+            if _supported_features(self.hass, light)
+        ]
+        to_replace = {CONF_LIGHTS: cv.multi_select(sorted(all_lights))}
 
         options_schema = {}
         for name, default, validation in VALIDATION_TUPLES:
