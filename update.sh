958a67d7ae0943e6022b3ed96a6aa1a1cac5cade
disrupted <hi@salomonpopp.me>
2021-03-22 23:37:11 +0100

    Include info for version install in update script

--- update.sh
+++ update.sh
@@ -4,5 +4,7 @@
 # sleep 5
 # echo "Installing Update if available"
 # sudo ha core update
+# to install a specific version run
+# sudo ha core update --version 2020.01.0
 git add .HA_VERSION
 git commit --no-verify -m "⬆️ Upgrade Home Assistant to $(cat .HA_VERSION)"
