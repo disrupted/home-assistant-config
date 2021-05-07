a0eaa8b7aae305ffd46e94bed196226a9a4c01e6
disrupted <hi@salomonpopp.me>
2021-05-07 17:08:07 +0200

    Set log level in update script

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 # echo "Checking for Update"
 # sudo ha supervisor reload
 # sleep 5
-# echo "Installing Update if available"
-# sudo ha core update
+echo "Installing Update if available"
+sudo ha core update --log-level Info
 # to install a specific version run
 # sudo ha core update --version 2020.01.0
 git add .HA_VERSION
